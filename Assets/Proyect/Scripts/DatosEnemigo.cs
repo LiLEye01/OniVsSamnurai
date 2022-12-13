e57166ee76bf4a7ee00513e9f0d9e8c88e3f25fc
Jorge Trujillo <a20310090@ucslp.net>
2022-12-13 00:53:15 -0600

    ok

--- Assets/Proyect/Scripts/DatosEnemigo.cs
+++ Assets/Proyect/Scripts/DatosEnemigo.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DatosEnemigo : MonoBehaviour
 {
     public int VidaEnemigo = 100;
+    public Slider vidaOni;
 
     private void Update() 
     
     {
+        vidaOni.value = VidaEnemigo;
+
         if (VidaEnemigo <= 0) 
         {
             Debug.Log("Die Oni");
